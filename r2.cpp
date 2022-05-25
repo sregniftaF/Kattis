cfaded77e3dc5001044c9f99ebefb9aa733d285c
sregniftaF <106218752+sregniftaF@users.noreply.github.com>
2022-05-25 11:42:53 +0800

    Add files via upload

---
+++ r2.cpp
@@ -0,0 +1,14 @@
+/* Note to self
+Using existing value of Mean 2 numbers, where 1 of the value is given
+Find the other value which was r2
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int r1, r2, s;
+	cin >> r1 >> s;
+	r2 = s * 2 - r1;
+	cout << r2 << endl;
+}
